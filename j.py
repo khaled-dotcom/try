b12bd1c3c5ea64d9a6c27c4056c0a299be943481
Khaled Ghalwash <115194209+khaled-dotcom@users.noreply.github.com>
2025-09-04 18:23:48 +0300

    Update j.py

--- j.py
+++ j.py
@@ -57,7 +57,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 # =====================
 # 3. Streamlit UI
 # =====================
-st.set_page_config(page_title="Laptop Chatbot", page_icon="💻", layout="centered")
+st.set_page_config(page_title="khaled Laptop t", page_icon="💻", layout="centered")
 st.title("💻 Laptop Recommendation Chatbot")
 st.write("Ask me anything about laptops!")
 
